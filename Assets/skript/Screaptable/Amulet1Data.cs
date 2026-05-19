5a5c252db5386d9104869e1fff5e90a016c64ffb
Deni-2012true <d29755026@gmail.com>
2026-05-19 17:42:46 +0300

    Reapply "Добавление инвентаря"
    This reverts commit 1acd1842e617d76e590c7f306b3cd97b2eafdd30.

---
+++ Assets/skript/Screaptable/Amulet1Data.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Amulet1Data", menuName = "Scriptable Objects/Amulet1Data")]
+public class Amulet1Data : ScriptableObject
+{
+    [Header("Название")]
+    public string name;
+
+    [Header("Ранг")]
+    public string rangName;
+    public int rangNumber; 
+
+    [Header("Защита")]
+    public string protectionName;
+    public int protectionNumber;
+
+    [Header("Особенности")]
+    public string feature;
+    public string featureName;
+    public int featureNumber; 
+
+    //Debuff 
+}
