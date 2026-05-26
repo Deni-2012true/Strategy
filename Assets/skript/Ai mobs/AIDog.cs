603fcf4080e1a6ea0c43f740957e7bf4df0d02ea
Deni-2012true <d29755026@gmail.com>
2026-05-26 18:04:57 +0300

    Атака волка

--- Assets/skript/Ai mobs/AIDog.cs
+++ Assets/skript/Ai mobs/AIDog.cs
@@ -10,9 +10,11 @@ public class AIDog : MonoBehaviour
 
     private Vector3 RandomDirection;
     private float changeDirectionTimer;
-    private float maxChange = 20f;
-    private float minChange = 17f;
+    private float maxChange = 14f;
+    private float minChange = 12f;
+
     private float walkTime = 4f;
+    private bool isWalking = true;
     
     void Start()
     {
@@ -22,22 +24,47 @@ public class AIDog : MonoBehaviour
 
     void Update()
     {
-        if (Vector3.Distance(transform.position, Player.position) <= 6)
+        if (Vector3.Distance(transform.position, Player.position) <= 6 && Vector3.Distance(transform.position, Player.position) > agent.stoppingDistance)
         {
+            isWalking = true; 
+            agent.isStopped = false;
+
             agent.speed = 3.5f;
             agent.SetDestination(Player.position);
             WolfAnimator.SetBool("Run", true);
-        }    
+        }  
+        else if (Vector3.Distance(transform.position, Player.position) <= agent.stoppingDistance && Vector3.Distance(transform.position, Player.position) >= 0)
+        {
+            WolfAnimator.SetBool("Walk", false);
+            WolfAnimator.SetBool("Run", false);
+            WolfAnimator.SetTrigger("Attack");
+        }  
         else
         {
             WolfAnimator.SetBool("Run", false);
-            agent.speed = 0.5f;
+            agent.speed = 1.1f;
+            
             changeDirectionTimer -= Time.deltaTime;
+
+            if (isWalking)
+            {
+                if (changeDirectionTimer <= (Random.Range(minChange, maxChange) - walkTime))
+                {
+                    isWalking = false;
+                    agent.isStopped = true;
+                    WolfAnimator.SetBool("Walk", false);
+                }
+                else
+                {
+                    WolfAnimator.SetBool("Walk", true);
+                    agent.SetDestination(transform.position + RandomDirection);
+                }
+            }
+
             if (changeDirectionTimer <= 0)
             {
                 ChangeDirection();
             }
-            agent.SetDestination(transform.position + RandomDirection);
         }
     }
 
@@ -45,5 +72,7 @@ public class AIDog : MonoBehaviour
     {
        RandomDirection = Random.insideUnitSphere * 10f;
        changeDirectionTimer = Random.Range(minChange, maxChange);
+       isWalking = true;
+       agent.isStopped = false;
     }
 }
